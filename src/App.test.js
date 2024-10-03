dff3cf3c12e520df9a20ad4eb21a7124c5797485
ruchi <ruchia401@gmail.com>
2024-10-03 18:49:30 +0530

    Initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Post from './components/Post'; // Adjust the path based on your file structure
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders post with user and text', () => {
+  render(<Post />);
+  const userElement = screen.getByText(/Massive_Mission_6386/i);
+  const textElement = screen.getByText(/I love my dog/i);
+  expect(userElement).toBeInTheDocument();
+  expect(textElement).toBeInTheDocument();
 });
